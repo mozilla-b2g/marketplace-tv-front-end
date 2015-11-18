3c3b34f1d06c598d26af94ed9e9814c86b201aa8
Joseph Yeh <jyeh@mozilla.com>
2015-11-18 14:11:57 +0800

    Bug 1225356 - Add spatial navigation to app list

--- src/media/js/views/homepage.js
+++ src/media/js/views/homepage.js
@@ -1,5 +1,23 @@
-define('views/homepage', ['core/l10n'], function(l10n) {
+define('views/homepage',
+    ['core/l10n', 'core/z', 'spatial-navigation'],
+    function(l10n, z, SpatialNavigation) {
     var gettext = l10n.gettext;
+
+    // Initialize spatial navigation.
+    SpatialNavigation.init();
+
+    // Define the navigable elements.
+    SpatialNavigation.add({
+        selector: '.focusable'
+    });
+
+    z.page.on('loaded', function() {
+        // Add 'tabindex="-1"' to "currently-existing" navigable elements.
+        SpatialNavigation.makeFocusable();
+
+        // Focus the first navigable element.
+        SpatialNavigation.focus();
+    });
 
     return function(builder) {
         builder.start('homepage.html');
