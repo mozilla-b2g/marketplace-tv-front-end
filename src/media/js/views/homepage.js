d1f23b8db40e1065e0f52d50445fe52e23ecab58
Joseph Yeh <jyeh@mozilla.com>
2015-12-02 15:20:18 +0800

    Bug 1227929 - Reskin app list

--- src/media/js/views/homepage.js
+++ src/media/js/views/homepage.js
@@ -26,8 +26,10 @@ define('views/homepage',
         var $appPreview = z.page.find('.app-preview');
         var $appPreviewPrice;
 
-        var focusedApp = appsModel.lookup($(this).data('slug'));
+        var focusedApp = appsModel.lookup(this.dataset.slug);
         var focusedManifestURL = focusedApp.manifest_url;
+
+        this.classList.add('focused');
 
         // Update app preview area with current focused app.
         $appPreview.html(
@@ -56,31 +58,60 @@ define('views/homepage',
         });
     });
 
-    z.page.on('sn:enter-down', '.focusable', function() {
-        if (!caps.webApps) {
+    z.page.on('keydown mousedown touchstart', '.focusable', function(e) {
+        if (e.type === 'keydown' && e.keyCode !== KeyEvent.DOM_VK_RETURN) {
             return;
         }
 
-        // Preview current focused app.
-        var focusedApp = appsModel.lookup($(this).data('slug'));
-        var focusedManifestURL = focusedApp.manifest_url;
+        this.classList.add('pressed');
+    });
 
-        apps.getInstalled().done(function(installedApps) {
-            var isInstalled = false;
+    z.page.on('keyup mouseup touchend', '.focusable', function(e) {
+        if (e.type === 'keyup' && e.keyCode !== KeyEvent.DOM_VK_RETURN) {
+            return;
+        }
 
-            // Check if app is installed.
-            installedApps.map(function(installedManifestURL) {
-                if (focusedManifestURL === installedManifestURL) {
-                    isInstalled = true;
+        this.classList.remove('pressed');
+        this.classList.add('released');
+
+        if (e.type === 'keyup') {
+            if (!caps.webApps) {
+                return;
+            }
+
+            // Preview current focused app.
+            var focusedApp = appsModel.lookup(this.dataset.slug);
+            var focusedManifestURL = focusedApp.manifest_url;
+
+            apps.getInstalled().done(function(installedApps) {
+                var isInstalled = false;
+
+                // Check if app is installed.
+                installedApps.map(function(installedManifestURL) {
+                    if (focusedManifestURL === installedManifestURL) {
+                        isInstalled = true;
+                    }
+                });
+
+                if (isInstalled) {
+                    apps.launch(focusedManifestURL);
+                } else {
+                    apps.install(focusedApp);
                 }
             });
+        }
+    });
 
-            if (isInstalled) {
-                apps.launch(focusedManifestURL);
-            } else {
-                apps.install(focusedApp);
-            }
-        });
+    z.page.on('transitionend', '.focusable', function() {
+        if (this.classList.contains('released')) {
+            this.classList.remove('released');
+        }
+    });
+
+    z.page.on('blur', '.focusable', function() {
+        this.classList.remove('focused');
+        this.classList.remove('pressed');
+        this.classList.remove('released');
     });
 
     z.page.on('contextmenu', '.focusable', function() {
